bcf1ff70481cde63483765bcec5fa9f22ea50fde
nhayden <nhayden@fhcrc.org>
2015-07-17 03:37:08 -0700

    remove defunct test for razip

--- inst/unitTests/test_compression.R
+++ inst/unitTests/test_compression.R
@@ -11,27 +11,10 @@ test_bgzip_openclose <- function()
     checkIdentical(0L, unlink(d, recursive=TRUE))
 }
 
+## NH 7/16/2015: I don't know what this is about, so I'm leaving it alone
 test_razip_small_files <- function()
 {
     src <- system.file("extdata", "ce2dict1.fa", package="Rsamtools")
     file.copy(src, dest <- tempfile())
     checkIdentical(readLines(src), readLines(dest))
-}
-
-test_razip_gzcompressed <- function()
-{
-    file.copy(system.file("extdata", "ce2dict1.fa", package="Rsamtools"),
-              src <- tempfile())
-    con <- gzfile(gzdest <- tempfile(), "wb")
-    writeLines(readLines(src), con, sep="\n")
-    close(con)
-    rzdest <- razip(gzdest)
-    checkIdentical(src, indexFa(src))
-    checkIdentical(gzdest, indexFa(gzdest))
-    checkIdentical(rzdest, indexFa(rzdest))
-    src <- scanFa(src, param=as(seqinfo(FaFile(rzdest)), "GRanges"))
-    rz <- scanFa(rzdest, param=as(seqinfo(FaFile(rzdest)), "GRanges"))
-    gz <- scanFa(rzdest, param=as(seqinfo(FaFile(rzdest)), "GRanges"))
-    checkIdentical(as.character(src), as.character(rz))
-    checkIdentical(as.character(src), as.character(gz))
 }
